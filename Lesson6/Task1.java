d65b014a0968db198bbbe780ad81cb5d1ecf0462
Matvey <radalert@mail.ru>
2022-12-29 20:43:34 +0300

    change Lesson6

--- Lesson6/Task1.java
+++ Lesson6/Task1.java
@@ -1,3 +1,10 @@
+package Lesson6;
+
 public class Task1 {
-    
+    public static void main(String[] args) {
+        Cat cat = new Cat(10);
+        cat.eat(12);
+        Cat cat1 = new Cat(10);
+        cat1.eat(5);
+    }
 }

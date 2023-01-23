fa0008ffabcf3a0136debae98209a36ccc8bf2fb
Matvey <radalert@mail.ru>
2023-01-23 22:39:30 +0300

    correct

---
+++ Lesson2/Human.java
@@ -0,0 +1,10 @@
+package Lesson2;
+
+public class Human  extends Participant{
+
+    public Human(String name, int distanceLimit, int jumpLimit, int swimLimit) {
+        super(name, distanceLimit, jumpLimit, swimLimit);
+        
+    }
+    
+}
